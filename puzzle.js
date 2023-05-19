9336b7dae08a57acc4a8abb36f00c47cf6ff3620
adithyaabinoy <adithyaabinoy@gmail.com>
2023-05-19 11:18:12 +0530

    puzzle_board

--- puzzle.js
+++ puzzle.js
@@ -11,8 +11,14 @@ var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 window.onload = function() {
     for (let r=0; r < rows; r++) {
         for (let c=0; c < columns; c++) {
-            
-            let tile = document.createElement
+
+            //<img id="0-0" src="1.jpeg">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg"; 
+
+            document.getElementById("board").append(tile);
+
         }
     }
 } 
