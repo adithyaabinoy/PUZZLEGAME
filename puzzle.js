3f087a5ffe956a677d76ec94397d28e9d3575f58
adithyaabinoy <adithyaabinoy@gmail.com>
2023-05-19 10:17:52 +0530

    css

---
+++ puzzle.js
@@ -0,0 +1,18 @@
+var rows = 3;
+var columns = 3;
+
+var currTile; //tile that you click to switch with .
+var otherTile; //the blank tile .
+
+var turns = 0;
+
+var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+window.onload = function() {
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++) {
+            
+            let tile = document.createElement
+        }
+    }
+} 
